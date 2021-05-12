c3e5b3a869d49315177dfe501a80a0b3cbd49006
HalilKesgin <halillkesgin@gmail.com>
2021-05-12 04:18:34 +0300

    First

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'article', component:ArticleComponent},
+  { path: 'about', component: AboutComponent },
+  { path: '', component: ArticleComponent }
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
